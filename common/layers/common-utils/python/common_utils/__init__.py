8157080cd2d1b5bcf41359ca97d18d5fe4b6c793
Koushik <koushiksinha12@gmail.com>
2025-06-29 15:07:36 -0400

    Add MilvusClient utility

--- common/layers/common-utils/python/common_utils/__init__.py
+++ common/layers/common-utils/python/common_utils/__init__.py
@@ -4,10 +4,15 @@ from .get_ssm import (
     parse_s3_uri,
     get_config,
 )
+from .milvus_client import MilvusClient, VectorItem, SearchResult, GetResult
 
 __all__ = [
     "get_values_from_ssm",
     "get_environment_prefix",
     "parse_s3_uri",
     "get_config",
+    "MilvusClient",
+    "VectorItem",
+    "SearchResult",
+    "GetResult",
 ]
